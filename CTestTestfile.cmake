c557c334979077bfcd8c1520161a36ad1cd9740d
schikin <sergey.chikin@toptal.com>
2019-08-26 22:02:19 +0300

    build updates

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vbccam/kms-omni-build/kms-jsonrpc
-# Build directory: /home/vbccam/kms-omni-build/kms-jsonrpc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- tests/CMakeFiles/test_rpc.check.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_rpc.check"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_rpc.check.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
